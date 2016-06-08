dfc936d7ca599f22c6a9396e1545631de40d630b
Marcello Passos <marcellogpassos@gmail.com>
2016-06-08 19:01:58 -0300

    Login funcionando

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -65,10 +65,10 @@ class AuthController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name'      => $data['name'],
-            'surname'   => $data['surname'],
-            'email'     => $data['email'],
-            'password'  => bcrypt($data['password']),
+            'name' => $data['name'],
+            'surname' => $data['surname'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
         ]);
     }
 }
