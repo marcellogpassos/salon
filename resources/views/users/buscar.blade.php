fdfb7bc94487d9aa07f9e790823d7e61882bfcb4
Marcello Galdino Passos <marcello.passos@tjpb.jus.br>
2016-06-21 12:01:52 -0300

    Mensagens na busca de usuários

--- resources/views/users/buscar.blade.php
+++ resources/views/users/buscar.blade.php
@@ -26,15 +26,25 @@
 
                                 <div class="input-field col s12 m6">
                                     <input id="nameInput" name="nome_sobrenome" type="text" maxlength="255"
-                                           value="{{ old('nome_sobrenome') ? old('nome_sobrenome') : $buscaPrevia['nome_sobrenome'] }}"
-                                           minlength="3" class="validate">
+                                           value="{{
+
+                                            old('nome_sobrenome') ?
+                                                old('nome_sobrenome') : isset($buscaPrevia) ?
+                                                    $buscaPrevia['nome_sobrenome'] : ""
+
+                                           }}" minlength="3" class="validate">
                                     <label for="nameInput">Nome ou sobrenome</label>
                                 </div>
 
                                 <div class="input-field col s12 m6">
-                                    <input id="emailInput" name="email" type="email" maxlength="255"
-                                           value="{{ old('email') ? old('email') : $buscaPrevia['email'] }}"
-                                           class="validate">
+                                    <input id="emailInput" name="email" type="email" maxlength="255" class="validate"
+                                           value="{{
+
+                                            old('email') ?
+                                                old('email') : isset($buscaPrevia) ?
+                                                    $buscaPrevia['email'] : ""
+
+                                            }}">
                                     <label for="emailInput">E-mail</label>
                                 </div>
 
@@ -46,12 +56,22 @@
                                     <div>
                                         <spam>
                                             <input name="sexo" type="radio" id="femininoInput" value="F"
-                                                    {!! (old('sexo') == 'F' || $buscaPrevia['sexo'] == 'F') ? ' checked' : '' !!}>
+                                                    {!!
+
+                                                        (old('sexo') == 'F' || (isset($buscaPrevia['sexo']) && $buscaPrevia['sexo'] == 'F')) ?
+                                                            ' checked' : ''
+
+                                                    !!}>
                                             <label for="femininoInput">Feminino</label>
                                         </spam>
                                         <spam>
                                             <input name="sexo" type="radio" id="masculinoInput" value="M"
-                                                    {!! (old('sexo') == 'M' || $buscaPrevia['sexo'] == 'M') ? ' checked' : '' !!}>
+                                                    {!!
+
+                                                        (old('sexo') == 'M' || (isset($buscaPrevia['sexo']) && $buscaPrevia['sexo'] == 'M')) ?
+                                                            ' checked' : ''
+
+                                                    !!}>
                                             <label for="masculinoInput">Masculino</label>
                                         </spam>
                                     </div>
@@ -60,13 +80,25 @@
 
                                 <div class="input-field col s12 m4">
                                     <input id="cpfInput" name="cpf" type="text" class="validate cpf"
-                                           value="{{ old('cpf') ? old('cpf') : $buscaPrevia['cpf'] }}">
+                                           value="{{
+
+                                            old('cpf') ?
+                                                old('cpf') : isset($buscaPrevia) ?
+                                                    $buscaPrevia['cpf'] : ""
+
+                                            }}">
                                     <label for="cpfInput">CPF</label>
                                 </div>
 
                                 <div class="input-field col s12 m4">
                                     <input id="telefoneInput" name="telefone" type="text" class="validate telefone"
-                                           value="{{ old('telefone') ? old('telefone') : $buscaPrevia['telefone'] }}">
+                                           value="{{
+
+                                            old('telefone') ?
+                                                old('telefone') : isset($buscaPrevia) ?
+                                                    $buscaPrevia['telefone'] : ""
+
+                                            }}">
                                     <label for="telefoneInput">Telefone</label>
                                 </div>
 
@@ -93,7 +125,22 @@
             </div>
         </div>
 
+        @if (isset($usersEncontrados) && (count($usersEncontrados) == 0))
+            <div id="information-alert" class="card card-alert card-alert-information">
+                <div class="card-content">
+                    <p>Consulta realizada com sucesso! Nenhum resultado encontrado.</p>
+                </div>
+            </div>
+        @endif
+
         @if(isset($usersEncontrados) && (count($usersEncontrados) > 0))
+
+            <div id="information-alert" class="card card-alert card-alert-information">
+                <div class="card-content">
+                    <p>Consulta realizada com sucesso! {{ count($usersEncontrados) }} resultado(s) encontrado(s).</p>
+                </div>
+            </div>
+
             <div class="row">
                 <div class="col s12">
 
