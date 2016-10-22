756546710c1eefd969a7320685f77803ba1eda7a
Marcello Galdino Passos <marcello.passos@tjpb.jus.br>
2016-10-22 11:38:42 -0300

    Validação front end do relatório de compras

---
+++ public/js/relatorioCompra.js
@@ -0,0 +1,117 @@
+var itemAutocompleteSelector = '.autocomplete.item';
+var itemHiddenInputSelector = '#itemHiddenInput';
+var clienteAutocompleteSelector = '.autocomplete.cliente';
+var clienteHiddenInputSelector = '#clienteHiddenInput';
+
+var buscarItemSrc = '{{url("/compras/buscarItem")}}';
+var buscarClienteSrc = '{{url("/compras/buscarCliente")}}';
+
+var codigoValidacaoInput = '#codigoValidacaoInput';
+
+var form = document.forms["relatorioComprasForm"];
+var campos = [
+    form['codigo_validacao'],
+    form['cliente'],
+    form['item'],
+    form['data_inicial'],
+    form['data_final'],
+    form['valor_minimo'],
+    form['valor_maximo']
+];
+
+function validateForm() {
+    return (
+        validarNenhumCampoFornecido(campos, function () {
+            showMessage('É necessário fornecer pelo menos um campo!');
+        }) &&
+        validarPrecedenciaDatas(form['data_inicial'].value, form['data_final'].value, function () {
+            showMessage('A data inicial deve ser anterior à data final!');
+            $('#dataInicialInput').val('');
+            $('#dataFinalInput').val('');
+        }) &&
+        validarRangeValores(form['valor_minimo'].value, form['valor_maximo'].value, function () {
+            showMessage('O valor mínimo deve ser menor que o valor máximo!');
+            $('#valorMinimoInput').val('');
+            $('#valorMaximoInput').val('');
+        })
+    );
+}
+
+var validarNenhumCampoFornecido = function (campos, errorCallback) {
+    for (i = 0; i < campos.length; i++)
+        if (campos[i].value)
+            return true;
+    errorCallback();
+    return false;
+};
+
+var validarPrecedenciaDatas = function (dataInicialString, dataFinalString, errorCallback) {
+    if (!dataInicialString || !dataFinalString)
+        return true;
+    var dataInicial = new Date(dataInicialString);
+    var dataFinal = new Date(dataFinalString);
+    if (dataInicial <= dataFinal)
+        return true;
+    errorCallback();
+    return false;
+};
+
+var validarRangeValores = function (valorMinimoString, valorMaximoString, errorCallback) {
+    if (!valorMinimoString || !valorMaximoString)
+        return true;
+    var valorMinimo = valorMinimoString.replace(".", "").replace(",", ".");
+    var valorMaximo = valorMaximoString.replace(".", "").replace(",", ".");
+    if (valorMinimo <= valorMaximo)
+        return true;
+    errorCallback();
+    return false;
+};
+
+$(itemAutocompleteSelector).autocomplete({
+    source: buscarItemSrc,
+    minLength: 2,
+    focus: function (event, ui) {
+        $(itemAutocompleteSelector).val(ui.item.label);
+        return false;
+    },
+    select: function (event, ui) {
+        $(itemAutocompleteSelector).val(ui.item.label);
+        $(itemHiddenInputSelector).val(ui.item.id);
+        return false;
+    },
+    change: function (event, ui) {
+        if (!ui.item) {
+            $(itemAutocompleteSelector).val('');
+            $(itemHiddenInputSelector).val('');
+        }
+    }
+});
+
+$(clienteAutocompleteSelector).autocomplete({
+    source: buscarClienteSrc,
+    minLength: 2,
+    focus: function (event, ui) {
+        $(clienteAutocompleteSelector).val(ui.item.label);
+        return false;
+    },
+    select: function (event, ui) {
+        $(clienteAutocompleteSelector).val(ui.item.label);
+        $(clienteHiddenInputSelector).val(ui.item.id);
+        return false;
+    },
+    change: function (event, ui) {
+        if (!ui.item) {
+            $(clienteAutocompleteSelector).val('');
+            $(clienteHiddenInputSelector).val('');
+        }
+    }
+});
+
+$(codigoValidacaoInput).change(function () {
+    var codigoFornecido = $(codigoValidacaoInput).val();
+    if (!validarCodigo(codigoFornecido)) {
+        $(codigoValidacaoInput).val('');
+        showMessage('O código deve conter 6 caracteres apenas letras e números');
+    } else
+        $(codigoValidacaoInput).val(codigoFornecido.toUpperCase());
+});
