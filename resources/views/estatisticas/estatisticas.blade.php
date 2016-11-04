4d3ee127f471dbff1e74f692190a9e822c2fe8f4
Marcello Passos <marcellogpassos@gmail.com>
2016-11-04 13:48:48 -0300

    Estatísticas agrupadas em tabs

--- resources/views/estatisticas/estatisticas.blade.php
+++ resources/views/estatisticas/estatisticas.blade.php
@@ -10,158 +10,192 @@
         @include('layouts.messages')
 
         <div class="row">
-            <div class="col s12 m6">
+
+            <div class="col s12">
 
                 <div class="card white">
 
-                    <h4 class="card-title">Clientes por Sexo</h4>
+                    <h4 class="card-title">Estat&iacute;sticas</h4>
 
-                    <div class="card-content gray-text text-darken-4">
-                        <div class="row">
-                            <div class="col offset-m1 m10 s12">
-                                <canvas id="clientesPorSexoChart" width="100%" height="100%"></canvas>
-                            </div>
-                        </div>
-                    </div>
+                    <div class="card-content">
 
-                </div>
-
-            </div>
-
-            <div class="col s12 m6">
-
-                <div class="card white">
-
-                    <h4 class="card-title">Clientes por Faixas Et&aacute;rias</h4>
-
-                    <div class="card-content gray-text text-darken-4">
-                        <div class="row">
-                            <div class="col offset-m1 m10 s12">
-                                <canvas id="clientesPorFaixaEtariaChart" width="100%" height="100%"></canvas>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div class="col s12 m6">
-
-                <div class="card white">
-
-                    <h4 class="card-title">Movimento semanal</h4>
-
-                    <div class="card-content gray-text text-darken-4">
-                        <div class="row">
-                            <div class="col offset-m1 m10 s12">
-                                <canvas id="movimentoSemanalChart" width="100%" height="100%"></canvas>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div class="col s12 m6">
-
-                <div class="card white">
-
-                    <h4 class="card-title">Movimento mensal</h4>
-
-                    <div class="card-content gray-text text-darken-4">
-                        <div class="row">
-                            <div class="col offset-m1 m10 s12">
-                                <canvas id="movimentoMensalChart" width="100%" height="100%"></canvas>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div class="col s12 m6">
-
-                <div class="card white">
-
-                    <h4 class="card-title">Servi&ccedil;os mais vendidos</h4>
-
-                    <div class="card-content gray-text text-darken-4">
                         <div class="row">
                             <div class="col s12">
-                                <table class="highlight bordered">
-                                    <thead>
-                                    <tr>
-                                        <th data-field="id">Nome do servi&ccedil;o</th>
-                                        <th data-field="name">Frequ&ecirc;ncia</th>
-                                    </tr>
-                                    </thead>
+                                <ul class="tabs tabs-fixed-width">
+                                    <li class="tab col s3"><a class="active" href="#clientesTab">Clientes</a></li>
+                                    <li class="tab col s3"><a href="#movimentoTab">Movimento</a></li>
+                                    <li class="tab col s3"><a href="#produtosServicosTab">Produtos e Servi&ccedil;os</a></li>
+                                </ul>
+                            </div>
+                            <div id="clientesTab" class="col s12">
+                                <div class="row">
+                                    <div class="col s12 m6">
 
-                                    <tbody>
-                                    @foreach($servicosMaisVendidos as $servico)
-                                        <tr>
-                                            <td>
-                                                <a href="{{ url('/servicos/buscar?id=' . $servico->servico_id) }}"
-                                                   class="special-link" target="_blank">
-                                                    {{ $servico->servico_id . ' - ' . $servico->servico_descricao }}
-                                                </a>
-                                            </td>
-                                            <td>{{ $servico->frequencia }}</td>
-                                        </tr>
-                                    @endforeach
-                                    </tbody>
-                                </table>
+                                        <div class="card white">
+
+                                            <h4 class="card-title">Clientes por Sexo</h4>
+
+                                            <div class="card-content gray-text text-darken-4">
+                                                <div class="row">
+                                                    <div class="col offset-m1 m10 s12">
+                                                        <canvas id="clientesPorSexoChart" width="100%" height="100%"></canvas>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+
+                                    <div class="col s12 m6">
+
+                                        <div class="card white">
+
+                                            <h4 class="card-title">Clientes por Faixas Et&aacute;rias</h4>
+
+                                            <div class="card-content gray-text text-darken-4">
+                                                <div class="row">
+                                                    <div class="col offset-m1 m10 s12">
+                                                        <canvas id="clientesPorFaixaEtariaChart" width="100%" height="100%"></canvas>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="movimentoTab" class="col s12">
+                                <div class="row">
+                                    <div class="col s12 m6">
+
+                                        <div class="card white">
+
+                                            <h4 class="card-title">Movimento semanal</h4>
+
+                                            <div class="card-content gray-text text-darken-4">
+                                                <div class="row">
+                                                    <div class="col offset-m1 m10 s12">
+                                                        <canvas id="movimentoSemanalChart" width="100%" height="100%"></canvas>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+
+                                    <div class="col s12 m6">
+
+                                        <div class="card white">
+
+                                            <h4 class="card-title">Movimento mensal</h4>
+
+                                            <div class="card-content gray-text text-darken-4">
+                                                <div class="row">
+                                                    <div class="col offset-m1 m10 s12">
+                                                        <canvas id="movimentoMensalChart" width="100%" height="100%"></canvas>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="produtosServicosTab" class="col s12">
+                                <div class="row">
+                                    <div class="col s12 m6">
+
+                                        <div class="card white">
+
+                                            <h4 class="card-title">Servi&ccedil;os mais vendidos</h4>
+
+                                            <div class="card-content gray-text text-darken-4">
+                                                <div class="row">
+                                                    <div class="col s12">
+                                                        <table class="highlight bordered">
+                                                            <thead>
+                                                            <tr>
+                                                                <th data-field="id">Nome do servi&ccedil;o</th>
+                                                                <th data-field="name">Frequ&ecirc;ncia</th>
+                                                            </tr>
+                                                            </thead>
+
+                                                            <tbody>
+                                                            @foreach($servicosMaisVendidos as $servico)
+                                                                <tr>
+                                                                    <td>
+                                                                        <a href="{{ url('/servicos/buscar?id=' . $servico->servico_id) }}"
+                                                                           class="special-link" target="_blank">
+                                                                            {{ $servico->servico_id . ' - ' . $servico->servico_descricao }}
+                                                                        </a>
+                                                                    </td>
+                                                                    <td>{{ $servico->frequencia }}</td>
+                                                                </tr>
+                                                            @endforeach
+                                                            </tbody>
+                                                        </table>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="card-action">
+                                                <a href="#">Ver todos</a>
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+
+                                    <div class="col s12 m6">
+
+                                        <div class="card white">
+
+                                            <h4 class="card-title">Produtos mais vendidos</h4>
+
+                                            <div class="card-content gray-text text-darken-4">
+                                                <div class="row">
+                                                    <div class="col s12">
+                                                        <table class="highlight bordered">
+                                                            <thead>
+                                                            <tr>
+                                                                <th data-field="id">Nome do produto</th>
+                                                                <th data-field="name">Frequ&ecirc;ncia</th>
+                                                            </tr>
+                                                            </thead>
+
+                                                            <tbody>
+                                                            @foreach($produtosMaisVendidos as $produto)
+                                                                <tr>
+                                                                    <td>
+                                                                        <a href="{{ url('/produtos/buscar?id=' . $produto->produto_id) }}"
+                                                                           class="special-link" target="_blank">
+                                                                            {{ $produto->produto_id . ' - ' . $produto->produto_descricao }}
+                                                                        </a>
+                                                                    </td>
+                                                                    <td>{{ $produto->frequencia }}</td>
+                                                                </tr>
+                                                            @endforeach
+                                                            </tbody>
+                                                        </table>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="card-action">
+                                                <a href="#">Ver todos</a>
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div class="card-action">
-                        <a href="#">Ver todos</a>
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div class="col s12 m6">
-
-                <div class="card white">
-
-                    <h4 class="card-title">Produtos mais vendidos</h4>
-
-                    <div class="card-content gray-text text-darken-4">
-                        <div class="row">
-                            <div class="col s12">
-                                <table class="highlight bordered">
-                                    <thead>
-                                    <tr>
-                                        <th data-field="id">Nome do produto</th>
-                                        <th data-field="name">Frequ&ecirc;ncia</th>
-                                    </tr>
-                                    </thead>
-
-                                    <tbody>
-                                    @foreach($produtosMaisVendidos as $produto)
-                                        <tr>
-                                            <td>
-                                                <a href="{{ url('/produtos/buscar?id=' . $produto->produto_id) }}"
-                                                    class="special-link" target="_blank">
-                                                    {{ $produto->produto_id . ' - ' . $produto->produto_descricao }}
-                                                </a>
-                                            </td>
-                                            <td>{{ $produto->frequencia }}</td>
-                                        </tr>
-                                    @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="card-action">
-                        <a href="#">Ver todos</a>
                     </div>
 
                 </div>
@@ -169,8 +203,6 @@
             </div>
 
         </div>
-
-
 
     </div>
 
