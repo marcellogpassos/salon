f9b0711ccb11bf87bd54be34fa0edefaf1ce2764
marcellogpassos <marcellogpassos@gmail.com>
2017-02-23 14:26:54 -0300

    inscreva-se

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -10,14 +9,10 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-
 Route::get('/', 'WelcomeController@welcome');
-
 Route::auth();
-
 Route::get('/home', 'HomeController@index');
-
+Route::get('/inscreva', 'UsersController@inscreva');
 Route::get('agenda/', 'AgendamentosController@minhaAgenda');
 Route::get('agendamentos/pendentes', 'AgendamentosController@agendamentosPendentes');
 Route::get('agendamentos/{id}', 'AgendamentosController@recuperarAgendamento');
@@ -25,9 +20,7 @@ Route::get('agendamentos/', 'AgendamentosController@index');
 Route::post('agendamentos/analisar', 'AgendamentosController@analisar');
 Route::post('agendamentos/', 'AgendamentosController@agendar');
 Route::delete('agendamentos/{id}', 'AgendamentosController@cancelarAgendamento');
-
 Route::get('categoriasServicos/{id}/servicos', 'ServicosController@listarServicosPorCategoria');
-
 Route::get('compras/buscarItem', 'ComprasController@buscarItem');
 Route::get('compras/buscarCliente', 'ComprasController@buscarCliente');
 Route::get('compras/buscar', 'ComprasController@buscarCompras');
@@ -36,21 +29,18 @@ Route::post('compras/registrar', 'ComprasController@registrarCompraAnonima');
 Route::post('compras/{codigoValidacao}/cancelar', 'ComprasController@cancelarCompra');
 Route::get('compras/{codigoValidacao}/detalhar', 'ComprasController@detalharCompra');
 Route::get('compras/{codigoValidacao}/emitirComprovante', 'ComprasController@emitirComprovanteCompra');
-
 Route::get('marcas/cadastrar', 'MarcasProdutosController@mostrarFormCadastrarMarcaProduto');
 Route::post('marcas/cadastrar', 'MarcasProdutosController@cadastrarMarcaProduto');
 Route::get('marcas/{id}/editar', 'MarcasProdutosController@mostrarFormEditarMarcaProduto');
 Route::post('marcas/{id}/editar', 'MarcasProdutosController@editarMarcaProduto');
 Route::get('marcas/{id}/excluir', 'MarcasProdutosController@excluirMarcaProduto');
 Route::get('marcas', 'MarcasProdutosController@mostrarListaMarcasProdutos');
-
 Route::get('produtos/buscar', 'ProdutosController@mostrarProdutosEncontrados');
 Route::get('produtos/cadastrar', 'ProdutosController@mostrarFormCadastrarProduto');
 Route::post('produtos/cadastrar', 'ProdutosController@cadastrarProduto');
 Route::get('produtos/{id}/editar', 'ProdutosController@mostrarFormEditarProduto');
 Route::post('produtos/{id}/editar', 'ProdutosController@editarProduto');
 Route::get('produtos/{id}/excluir', 'ProdutosController@excluirProduto');
-
 Route::get('servicos/buscar', 'ServicosController@mostrarServicosEncontrados');
 Route::get('servicos/cadastrar', 'ServicosController@mostrarFormCadastrarServico');
 Route::post('servicos/cadastrar', 'ServicosController@cadastrarServico');
@@ -58,7 +48,6 @@ Route::get('servicos/{id}/editar', 'ServicosController@mostrarFormEditarServico'
 Route::post('servicos/{id}/editar', 'ServicosController@editarServico');
 Route::get('servicos/{id}/excluir', 'ServicosController@excluirServico');
 Route::get('servicos/{id}/profissionais', 'ServicosController@listarProfissionaisPorServico');
-
 Route::post('users/status', 'UsersController@status');
 Route::get('users/buscar', 'UsersController@mostrarUsuariosEncontrados');
 Route::get('users/cadastrar', 'UsersController@mostrarFormCadastrarCliente');
@@ -76,12 +65,8 @@ Route::post('users/{id}/papeis', 'UsersController@editarPapeis');
 Route::get('users/{id}/registrarCompra', 'ComprasController@mostrarFormRegistrarCompra');
 Route::post('users/{id}/registrarCompra', 'ComprasController@registrarCompra');
 Route::get('users/{id}', 'UsersController@recuperarUsuario');
-
 Route::get('estatisticas/', 'EstatisticasController@mostrarEstatisticas');
-
 Route::get('ufs/{uf}/municipios', 'EnderecosController@listarMunicipios');
 Route::get('municipios/{municipio}', 'EnderecosController@getMunicipio');
-
 Route::post('mensagens/', 'MensagensController@enviarMensagem');
-
 Route::get('index', 'WebsiteController@index');
